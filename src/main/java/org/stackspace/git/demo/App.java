5fdd7ae0f09a853d623804b76587c958152874b1
deba <debacool@gmail.com>
2018-09-10 11:12:27 +0530

    added git demo project to local repo

---
+++ src/main/java/org/stackspace/git/demo/App.java
@@ -0,0 +1,11 @@
+package org.stackspace.git.demo;
+
+public class App {
+	public void addData(String data) {
+		System.out.println("data:" + data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
